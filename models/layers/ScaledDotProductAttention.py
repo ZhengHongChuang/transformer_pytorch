7f03c277d3e810cd1df8963fc29da61021884ebe
HongChuang Zheng <2505460258@qq.com>
2024-04-30 16:40:54 +0800

    init ScaleDotProductAttention

---
+++ models/layers/ScaledDotProductAttention.py
@@ -0,0 +1,19 @@
+import math
+import torch.nn as nn
+
+class ScaleDotProductAttention(nn.Module):
+    def __init__(self) :
+        super(ScaleDotProductAttention,self).__init__()
+        self.softmax = nn.Softmax(dim=1)
+    def foward(self,q,k,v,mask = None,e=1e-12):
+        """
+            k的形状 : [batch_size,head,max_len,d_tensor]
+        """
+        _,_,_,d_tensor = k.size()
+        k_t = k.transpose(2,3)
+        score = (q@k_t)/math.sqrt(d_tensor)
+        if mask is not None:
+            score.masker_fill(mask==0,e)
+        score = self.softmax(score)
+        v = score @ v
+        return v ,score
