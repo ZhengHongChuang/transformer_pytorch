fae4044f3e84f4e5faac450d530463d196b2a3f5
HongChuang Zheng <2505460258@qq.com>
2024-04-30 17:20:05 +0800

    init PositionwiseFeedForward

---
+++ models/layers/PositionwiseFeedForward.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class PositionwiseFeedForward(nn.Module):
+    """
+        transformer 的前馈网络模块
+    """
+    def __init__(self,d_model,hidden,drop_prob = 0.1) :
+        super(PositionwiseFeedForward,self).__init__()
+        self.linear1 = nn.Linear(d_model,hidden)
+        self.linear2 = nn.Linear(hidden,d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(drop_prob)
+    def forward(self,x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
